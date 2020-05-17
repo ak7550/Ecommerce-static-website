61d7323070a979c85b12b86213aa73d15a056899
Aniket Ghosh <ghoshaniketkumar7@gmail.com>
2020-05-17 13:03:23 +0530

    cool things

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,14 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Footer from './reusable/Footer';
+import Navbar from './reusable/Navbar';
+
 import './bootstrap.min.css';
 import "./layout.css"
 
 const Layout = ({ children }) => (
   <>
+    <Navbar />
     {children}
     <Footer />
   </>
