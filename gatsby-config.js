f35a2b3a4a974a418d7c5a2ad9d0ae5fcda419be
Aniket Ghosh <ghoshaniketkumar7@gmail.com>
2020-05-17 10:25:57 +0530

    basic changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Yoututbe Project`,
+    description: `1st Project on Gatsby.`,
+    author: `ak@gmail.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
